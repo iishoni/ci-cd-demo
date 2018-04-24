3d5ae07ab9d0048a63052585da3494e68439f221
dxk19911212 <648638730@qq.com>
2018-04-24 16:44:36 +0800

    first commit

---
+++ src/main/java/com/iishoni/web/DemoController.java
@@ -0,0 +1,27 @@
+package com.iishoni.web;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/{name}")
+    public String hello(@PathVariable String name) {
+        return "hello " + name;
+    }
+
+    @PostMapping
+    public String save() {
+        return "保存成功！";
+    }
+
+    @PutMapping
+    public String update() {
+        return "更新成功！";
+    }
+
+    @DeleteMapping
+    public String delete() {
+        return "删除成功！";
+    }
+}
